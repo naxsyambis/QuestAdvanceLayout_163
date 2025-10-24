c102ae00c09ea78008bf598010777b8baf018378
naxsyambis <naxsyambis@gmail.com>
2025-10-24 15:40:31 +0700

    membuat row di dalam padding dengan center vertically dan memberikan padding 10

--- app/src/main/java/com/example/questadvancelayout_163/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_163/UIPertama.kt
@@ -1,6 +1,7 @@
 package com.example.questadvancelayout_163
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -46,6 +47,11 @@ fun ActivitasPertama(modifier: Modifier){
             colors = CardDefaults.cardColors(
                 containerColor = Color.DarkGray
             )
-        ){}
+        ){
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(all = 10.dp)
+            ){}
+        }
     }
 }
