9e081b4bb1c95b5e8102db95c8db939ac0dc0186
naxsyambis <naxsyambis@gmail.com>
2025-10-24 15:08:20 +0700

    membuat file UIPertama

---
+++ app/src/main/java/com/example/questadvancelayout_163/UIPertama.kt
@@ -0,0 +1,7 @@
+package com.example.questadvancelayout_163
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import java.lang.reflect.Modifier
+
