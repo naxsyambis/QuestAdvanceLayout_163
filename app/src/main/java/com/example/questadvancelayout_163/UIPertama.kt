90fd97649afa9b64dbc407479e09f21d8641af4c
naxsyambis <naxsyambis@gmail.com>
2025-10-24 15:13:14 +0700

    memperbaiki fungsi yang tidak terbaca

--- app/src/main/java/com/example/questadvancelayout_163/UIPertama.kt
+++ app/src/main/java/com/example/questadvancelayout_163/UIPertama.kt
@@ -1,15 +1,20 @@
 package com.example.questadvancelayout_163
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import java.lang.reflect.Modifier
 
 @Composable
-fun ActivitasPertama(modifier: Modifier) {
+fun ActivitasPertama(modifier: Modifier){
     Column(modifier= Modifier
         .padding(top=100.dp)
         .fillMaxSize(),
-        horizontalAligmnet = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally
     )
 }
