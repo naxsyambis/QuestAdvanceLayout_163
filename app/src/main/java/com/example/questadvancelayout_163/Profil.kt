ddcb94f487fea503577e0c71e927e75631769304
naxsyambis <naxsyambis@gmail.com>
2025-10-25 13:01:12 +0700

    Menambahkan image saya kedalam drawable

--- app/src/main/java/com/example/questadvancelayout_163/Profil.kt
+++ app/src/main/java/com/example/questadvancelayout_163/Profil.kt
@@ -1,13 +1,20 @@
 package com.example.questadvancelayout_163
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -18,5 +25,7 @@ fun Profil(modifier: Modifier = Modifier) {
             .background(Color.Black)
             .padding(top = 100.dp),
         horizontalAlignment = Alignment.CenterHorizontally
-    ){}
+    ){
+
+    }
 }
