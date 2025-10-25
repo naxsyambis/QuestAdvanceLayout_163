fe6740431b41dc1d289cb6fb094dc8af15f77dc6
naxsyambis <naxsyambis@gmail.com>
2025-10-25 13:15:53 +0700

    menambahkan spacer dan membuatkan row yang akan berisi icon

--- app/src/main/java/com/example/questadvancelayout_163/Profil.kt
+++ app/src/main/java/com/example/questadvancelayout_163/Profil.kt
@@ -3,8 +3,12 @@ package com.example.questadvancelayout_163
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -36,5 +40,11 @@ fun Profil(modifier: Modifier = Modifier) {
             contentScale = ContentScale.Crop
         )
 
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(24.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){}
     }
 }
