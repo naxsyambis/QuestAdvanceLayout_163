44f00d49d4ab6fd03a0a932869cb10b83b49d793
naxsyambis <naxsyambis@gmail.com>
2025-10-25 18:37:06 +0700

    membuat card pertama

--- app/src/main/java/com/example/questadvancelayout_163/Profil.kt
+++ app/src/main/java/com/example/questadvancelayout_163/Profil.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -105,6 +108,27 @@ fun Profil(modifier: Modifier = Modifier) {
             )
 
             Spacer(modifier = Modifier.height(25.dp))
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.Gray)
+            ){
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 10.dp, vertical = 5.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ){
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+
+                    }
+                }
+            }
         }
     }
 }
